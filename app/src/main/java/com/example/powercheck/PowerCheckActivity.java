76d64c8630476e41b9221b09de67268ec2678ee9
admin <wwww>
2021-08-25 15:37:59 +0800

    .

--- app/src/main/java/com/example/powercheck/PowerCheckActivity.java
+++ app/src/main/java/com/example/powercheck/PowerCheckActivity.java
@@ -1,10 +1,13 @@
 package com.example.powercheck;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
-import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,7 @@ import androidx.core.app.ActivityCompat;
 import com.example.lapace.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PowerCheckActivity extends AppCompatActivity {
@@ -24,37 +28,76 @@ public class PowerCheckActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power_check);
 
-        Button button = findViewById(R.id.read_write);
-        button.setOnClickListener(view -> {
-            getPower();
+
+        //读写权限验证的click事件
+        findViewById(R.id.read_write).setOnClickListener(view -> {
+            String[] checkList = new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            };
+            checkSetJurisdiction(checkList);
+        });
+
+
+        //全局弹窗权限的click事件
+        /**
+         * 判断是否有全局弹窗的权限，如果没有则跳转到设置界面。
+         */
+        findViewById(R.id.alert_global).setOnClickListener(view -> {
+            if (! Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "浮窗权限,请前往设置", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent,10);
+            }
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SYSTEM_ALERT_WINDOW} ,200);
         });
     }
 
-    private void getPower() {
-        String[] checkList = new String[]{
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        };
+    /**
+     * 判断权限是否拥有，并申请没有的权限
+     * @param JurisdictionList
+     */
+    private void checkSetJurisdiction(String [] JurisdictionList) {
         //判断那些权限没有
         List<String> list = new ArrayList<>();
-        for (String needPower:checkList) {
-            if(PackageManager.PERMISSION_GRANTED!= ActivityCompat.checkSelfPermission(getApplicationContext(),needPower)){
-                list.add(needPower);
+        for (String jd:JurisdictionList) {
+            if(PackageManager.PERMISSION_GRANTED!= ActivityCompat.checkSelfPermission(getApplicationContext(),jd)){
+                list.add(jd);
+            }else {
+                Toast.makeText(this, "该权限已授予", Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "该权限已授予: "+jd);
             }
         }
-
         //申请权限
         if(list.size() > 0){
             String[] strings = list.toArray(new String[list.size()]);
             ActivityCompat.requestPermissions(this,strings,100);
         }
-
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        Log.e(TAG, "onRequestPermissionsResult: " + String.valueOf(requestCode) );
+        /*
+        requestCode: ActivityCompat.requestPermissions中设置的requestCode
+        permissions: 权限名称
+        grantResults: 【拒绝】-1 【允许】0
+         */
+        switch(requestCode){
+            case 100:
+                Log.e(TAG, "读写权限的申请" );
+                break;
+            case 200:
+                Log.e(TAG, "全局弹窗的权限验证" );
+        }
+        if(grantResults[0] == 0){
+            Toast.makeText(this,"感谢您的信任", Toast.LENGTH_SHORT).show();
+        }else{
+
+        }
+        Log.e(TAG, "requestCode: " + requestCode);
+        Log.e(TAG, "grantResults: " + Arrays.toString(grantResults));
     }
 
     @Override
