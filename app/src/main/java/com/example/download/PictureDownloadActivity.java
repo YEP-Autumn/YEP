26fa1f106fcfac363bd4f907ba860cb21c870dfb
admin <wwww>
2021-09-07 18:42:22 +0800

    break

--- app/src/main/java/com/example/download/PictureDownloadActivity.java
+++ app/src/main/java/com/example/download/PictureDownloadActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.Adapter.AdapterDemo;
@@ -14,6 +15,7 @@ import com.example.http.RetrofitInterface;
 import com.example.lapace.R;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,7 +30,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class PictureDownloadActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "YEP";
-//    private String url = "https://tse1-mm.cn.bing.net/th/id/R-C.06ce7237f905735df8cfcff44d6a11fa?rik=YruL7uefVPKMiQ&riu=http%3a%2f%2fpic37.nipic.com%2f20140104%2f3970232_165407697000_2.jpg&ehk=AWvGiGn1EQJjR23BP%2f9G0i21Nqr64moWF%2fjETBK%2fjvY%3d&risl=&pid=ImgRaw&r=0";
+    //    private String url = "https://tse1-mm.cn.bing.net/th/id/R-C.06ce7237f905735df8cfcff44d6a11fa?rik=YruL7uefVPKMiQ&riu=http%3a%2f%2fpic37.nipic.com%2f20140104%2f3970232_165407697000_2.jpg&ehk=AWvGiGn1EQJjR23BP%2f9G0i21Nqr64moWF%2fjETBK%2fjvY%3d&risl=&pid=ImgRaw&r=0";
     private String url = "https://www.baidu.com/";
     ListView listView;
 
@@ -41,7 +43,11 @@ public class PictureDownloadActivity extends AppCompatActivity implements View.O
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(PictureDownloadActivity.this, i + " " + l + " :)", Toast.LENGTH_SHORT).show();
+                TextView textView = view.findViewById(R.id.item_text);
+                String s = new StringBuilder().append("我是第").append(i).append("个Item")
+                        .append("  ").append("我被点击了")
+                        .append("  ").append(":)").toString();
+                textView.setText(s);
             }
         });
 
@@ -52,12 +58,16 @@ public class PictureDownloadActivity extends AppCompatActivity implements View.O
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.start_picture_download:
-                List<String> list = Arrays.asList("0","1","2","3","4","5","6","7","8","9");
-                listView.setAdapter(new AdapterDemo(getApplicationContext(),list,Arrays.asList(R.drawable.icon)));
+                List<String> list = new ArrayList<>();
+                for (int i = 0; i < 20; i++) {
+                    list.add(String.valueOf(i));
+                }
+                listView.setAdapter(new AdapterDemo(getApplicationContext(), list, Arrays.asList(R.drawable.icon)));
                 break;
         }
     }
-    private Integer fun(){
+
+    private Integer fun() {
         OkHttpClient okHttpClient = new OkHttpClient();
         Request.Builder builder = new Request.Builder()
                 .url("https://tse1-mm.cn.bing.net/th/id/R-C.06ce7237f905735df8cfcff44d6a11fa?rik=YruL7uefVPKMiQ&riu=http%3a%2f%2fpic37.nipic.com%2f20140104%2f3970232_165407697000_2.jpg&ehk=AWvGiGn1EQJjR23BP%2f9G0i21Nqr64moWF%2fjETBK%2fjvY%3d&risl=&pid=ImgRaw&r=0")
